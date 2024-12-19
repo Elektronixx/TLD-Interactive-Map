0ad1f7fd4f69da4678b6d713eea4ad84381ed6f9
Elektronixx <37477610+Elektronixx@users.noreply.github.com>
2024-12-19 22:20:16 +0100

    add links

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,90 @@ let currentCategory = 'pilgrim'; // Default to Pilgrim category
 
 // Map images for each map and difficulty
 const maps = {
-    'mystery-lake': {
+    'forlorn-muskeg': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481323974/A8EDACD01C767BEDF3FA26B106079FF66A2FF53F/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481324566/5A3CA51E9B96CC0DDBA74E34C5887433456E42B0/'
+    },
+	'broken-railroad"': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481696066/8674195A3C3098E9878C83512BF03B9CF9488AA9/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481696468/C355359C5487B23BF2BBD00EFE4CEA054573F085/'
+    },
+	'mountain-town': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481699017/E90C0142CCAC23DA2C6C3276ED5C5B495B558E3B/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481699464/16B898F15338D2A5ED837903C88B419569547E32/'
+    },
+	'hushed-river-valley': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481697948/7AA0DAB2DF429E9628DB12B7925FD7FFEF0199AD/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481698404/CAA4C361027CA65ED8E6FE305F8BC878C60F87B7/'
+    },
+	'keepers-pass': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481276747/9690CC6CDA2BB2869FAF70E8F2E219986D5FC52D/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481277043/6EFDAE3B58692376CA0E9A5A912846FA1AD7CC09/'
+    },
+	'bleak-inlet': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481694098/3251230F92F7B16675334CCDF8159FFB4F524D7C/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481694671/37F0FDFC96EC2575E51051571163D20BE40A36D5/'
+    },
+	'the-ravine': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481701532/3E574FDCD020EC941832AACB3714FDF23ED3FD78/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481702024/B5B784C90D6D2D080F98515DA61F621009947340/'
+    },
+	'winding-river': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481283290/2F95CDD280AF1BD5D52072DD0B242519C099F433/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481283760/CD74A44711B35D34DC967262E7E90ECAA227B999/'
+    },
+	'pleasant-valley': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481699824/C9745785ADF9844B76B23DB10D06B151EC80114B/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481700323/6CB3406F97128B65D6508B26FFF117A1221D4EFE/'
+    },
+	'timberwolf-mountain': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481400687/BC687FC6F0DFC0CF49BD570CC2E3DF82B3FDEA78/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481329519/EAC7153F23C219946789171B226556868ED2614E/'
+    },
+	'ash-canyon': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481693149/70429F0153C75BC7400704874EEB7891AB33573C/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481693702/16EF030EB787CBC744D8AEC61D4D85F8EC35DE7F/'
+    },
+	'coastal-highway': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481696951/E170F04CF9718C9422D70BDFA240C5832746D391/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481697237/BCE9865344B16E72C843274B0F413082B4CE9AFD/'
+    },
+	'crumbling-highway': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481274812/342BCF2DFFECDB3DDA0D51D4ABB53E4B6C9BCB5A/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481274812/342BCF2DFFECDB3DDA0D51D4ABB53E4B6C9BCB5A/'
+    },
+	'desolation-point': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481332331/1B0B936FB2CEA055BA1C3951C350CDE7B102E2EF/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481332746/E72BAE7FA29DBF76876A59063FF5145FA77FA4EB/'
+    },
+	'mystery-lake': {
         'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481350791/07616FEC9E85DC8552164D0D75C42F8B25DFEA7A/',
         'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481351403/DF78A6A5A0F8766BF9AE6D44A7AC658EA6D75A48/'
     },
-    'ash-canyon': {
-        'pilgrim': 'https://example.com/ash-canyon-pilgrim.jpg',
-        'interloper': 'https://example.com/ash-canyon-interloper.jpg'
+	'blackrock': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745483478462/F4AF10AC39344BC5FDB2D5464E72573D20817579/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481695550/71900725DE5594C519ECFD95DFBF5A8C7F1AED2C/'
     },
-    'broken-rail': {
-        'pilgrim': 'https://example.com/broken-rail-pilgrim.jpg',
-        'interloper': 'https://example.com/broken-rail-interloper.jpg'
+	'far-range-branch-line': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37812669431422742/CD9AF43DCC69CEDC91A49FDEC6E3F3E125049FAD/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37812669431423046/0306E8179F0F09FF0E9DC6AFB823FEB33D62AEF1/'
     },
-    // Add other maps here...
+	'transfer-pass': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481303604/2F143B1F4452AE9F9688686307AEAFE77D64CD0C/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481303971/1A9515074C6E2DFE5960A3886C0534BB20265B41/'
+    },
+	'forsaken-airfield': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481300633/4D0E18C081174598B9D17AC8E170F0354E7BCC30/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481300997/57442ABAEB646FC2C6BF1AC62DE7D8E84F41D71D/'
+    },
+	'sundered-pass': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481293800/B618B2F10F9E696C9C9E97DD038F0B75C6ECC331/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481294355/36AAC75152B0D618BE009C5AFD7FDBCD37CA17BD/'
+    },
+	'zone-of-contamination': {
+        'pilgrim': 'https://steamuserimages-a.akamaihd.net/ugc/37814745481297380/BC0F009376B5F60EB884E903629FC6DCBC43A9A2/',
+        'interloper': 'https://steamuserimages-a.akamaihd.net/ugc/37814745485571671/C6D652B001FDB57F28C1AE94175A12FD564C4AC6/'
+    },
 };
 
 // Function to toggle the difficulty category
@@ -162,7 +233,3 @@ function showStartMap() {
         image.style.top = '0px';
     });
 }
-
-const img = document.querySelector('#start-map-image');
-console.log('Natural size:', img.naturalWidth, img.naturalHeight);
-console.log('Rendered size:', img.clientWidth, img.clientHeight);
